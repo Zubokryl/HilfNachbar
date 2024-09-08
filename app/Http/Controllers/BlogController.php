638ec063ab2fa4cdb953e242a6a235b799e92e40
Zubokryl <zubokryl@gmail.com>
2024-09-09 00:14:03 +0200

    added files in Providers

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    public function blog() {
+        return 'You have passed the autherisation';
+    }
+}
