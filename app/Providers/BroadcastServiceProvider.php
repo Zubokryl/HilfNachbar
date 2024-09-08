ba63a478d6402aa76e23278e30d345ee25e0885a
Zubokryl <zubokryl@gmail.com>
2024-09-08 23:54:58 +0200

    Добавлены недостающие файлы и папки

---
+++ app/Providers/BroadcastServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Broadcasting\BroadcastServiceProvider as ServiceProvider;
+
+class BroadcastServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any broadcasting services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
